2bb2147b39bbd87be8b897fc5ec17836bc653f23
Md. Rakibul Islam <rakib1708@gmail.com>
2021-06-04 15:20:54 +0600

    Added axios and vue-router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import Axios from 'axios';
+import store       from './store';
+import router      from './router';
+
+const app = createApp(App);
+app.config.globalProperties.$http = Axios;
+app.config.globalProperties.$http.defaults.headers.common['Content-Type'] = 'application/json';
+app.config.globalProperties.$http.defaults.headers.common['Origin'] = '*';
+const token = localStorage.getItem('token')
+if (token) {
+	app.config.globalProperties.$http.defaults.headers.common['Authorization'] = 'bearer '+token;
+}
+
+app.config.devtools = true;
+app
+.use(router)
+.use(store)
+.mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+//import axios from "axios";
+
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/Home.vue')
+    }
+]
+
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
